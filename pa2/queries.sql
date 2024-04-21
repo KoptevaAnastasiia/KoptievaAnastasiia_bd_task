5aa290fed302680c3df5b415661440fb33d2c38a
KoptevaAnastasiia <150849726+KoptevaAnastasiia@users.noreply.github.com>
2024-04-21 23:57:56 +0300

    update

---
+++ pa2/queries.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA my_test;
+USE my_test;
+
+-- Select distinct massage therapists' names who work with massage services but are not working on a specific date.
+SELECT DISTINCT e.name AS massage_therapist
+FROM employee e
+         JOIN employee_service es ON e.employee_id = es.employee_id
+WHERE es.service_id IN (SELECT id FROM service WHERE name LIKE '%Massage%')
+  AND e.employee_id NOT IN (SELECT DISTINCT a.employee_id FROM appointment a WHERE DATE(a.datetime) BETWEEN '2004-03-21' AND '2025-03-27');
+
+-- Select product_name and count of its usage within a specified date range.
+SELECT p.name AS product_name, COUNT(sp.product_id) AS required_products
+FROM appointment a
+         JOIN service s ON a.service_id = s.id
+         JOIN services2products sp ON s.id = sp.service_id
+         JOIN product p ON sp.product_id = p.id
+WHERE DATE(a.datetime) BETWEEN '2004-03-21' AND '2025-03-27'
+GROUP BY s.id, s.name, p.id, p.name;
--- pa5/views.sql
+++ pa5/views.sql
@@ -1,35 +1,36 @@
 USE my_test;
 
-CREATE TABLE employee__schedule (
+CREATE TABLE employee_schedule1 (
                                     schedule_id INT PRIMARY KEY AUTO_INCREMENT,
                                     employee_id INT,
                                     FOREIGN KEY (employee_id) REFERENCES employee(employee_id),
                                     start_time TIME,
                                     end_time TIME,
-                                    service_type VARCHAR(255)
+                                    service_type VARCHAR(255),
+                                    service_list VARCHAR(1024)  
 );
- 
 
-INSERT INTO employee__schedule (employee_id, start_time, end_time, service_type)
+ INSERT INTO employee_schedule1 (employee_id, start_time, end_time, service_type, service_list)
 SELECT
     es.employee_id,
-    s.start_time,
-    s.end_time,
-    s.name AS service_type
+    MIN(s.start_time) AS start_time,
+    MAX(s.end_time) AS end_time,
+    s.name AS service_type,
+    GROUP_CONCAT(DISTINCT s.name ORDER BY s.name SEPARATOR ', ') AS service_list
 FROM
     employee_service es
-        JOIN
-    service s ON es.service_id = s.id;
-
+        JOIN service s ON es.service_id = s.id
+GROUP BY
+    es.employee_id, s.name;
 
 SELECT
     e.name AS 'Employee name',
-        TIME_FORMAT(s.start_time, '%H:%i') AS 'Start Time',
-        TIME_FORMAT(s.end_time, '%H:%i') AS 'End Time',
-        s.name AS 'Kind of service'
+    TIME_FORMAT(es1.start_time, '%H:%i') AS 'Start Time',
+    TIME_FORMAT(es1.end_time, '%H:%i') AS 'End Time',
+    es1.service_type AS 'Kind of service',
+    es1.service_list AS 'Service List'
 FROM
     employee e
-        JOIN
-    employee_service es ON e.employee_id = es.employee_id
-        JOIN
-    service s ON es.service_id = s.id;
+        JOIN employee_schedule1 es1 ON e.employee_id = es1.employee_id
+ORDER BY
+    e.name, es1.start_time;
